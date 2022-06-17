43c50072920960bcff0041815ed5023368328d8c
Dizio <37836244+Dizi0@users.noreply.github.com>
2022-06-17 16:49:43 +0200

    Splitting files, adding preference system

--- content.js
+++ content.js
@@ -1,5 +1,13 @@
 /* Listen for messages */
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+    let preferences = msg.data.Preferences
+
+    if (localStorage.getItem('SbblTchatModPreferences') === undefined) {
+        localStorage.setItem('SbblTchatModPreferences', JSON.stringify(preferences));
+    } else if (JSON.stringify(preferences) !== localStorage.getItem('SbblTchatModPreferences') ) {
+        localStorage.setItem('SbblTchatModPreferences', JSON.stringify(preferences))
+    }
+
     if (msg.command && (msg.command === "triggerTchatMod")) {
         const r = document.querySelector(':root');
         const navbar = document.querySelector('.nav');
@@ -8,62 +16,75 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 
         document.querySelector('.simplebar-content-wrapper').setAttribute('id', 'msgBox');
         document.querySelector('.simplebar-content').setAttribute('id', 'scrollSensor');
+
         let sensor = document.getElementById('scrollSensor');
 
         sensor.addEventListener("DOMNodeInserted", function(e) {
-            const objDiv = document.getElementById("msgBox");
-            objDiv.scrollTop = objDiv.scrollHeight;
+            if (preferences.autoScroll === true) {
+                const objDiv = document.getElementById("msgBox");
+                objDiv.scrollTop = objDiv.scrollHeight;
+            }
+
             const rawMsg =  e.target.innerText.toLowerCase().split(': ')
             const msgSender = rawMsg[0].replace(/^\s+|\s+$/g, '');
             const msgSent = rawMsg[1]
 
             if (msgSender === nickname) {
-                e.target.style.color = '#00bcd4'
+                e.target.style.color = preferences.myMsgColor
             } else if (msgSender.includes('mp de ')) {
-                mpNotifSound.play();
+                if (preferences.notifSound === true) {
+                    mpNotifSound.play();
+                }
             } else if (msgSent.includes(nickname)) {
-                e.target.style.color = '#18CB00FF';
-                mpNotifSound.play();
+                e.target.style.color = preferences.pingMsg;
+                if (preferences.notifSound === true) {
+                    mpNotifSound.play();
+                }
             }
         }, false);
 
-        const sheet = new CSSStyleSheet();
 
-        sheet.replaceSync('.text-blabla {color: white}');
+        if (preferences.nightMode === true) {
+            const sheet = new CSSStyleSheet();
 
-        let body = document.querySelector('body');
+            sheet.replaceSync('.text-blabla {color: white}');
 
-        // Apply the stylesheet to a document:
-        document.adoptedStyleSheets = [sheet];
-        body.style.backgroundColor = '#263238'
-        body.style.setProperty('background-image', 'unset');
-        navbar.style.setProperty('position', 'unset');
-        r.style.setProperty('--blue', 'lightblue');
-        r.style.setProperty('--clr-white', '#303030');
-        r.style.setProperty('--clr-black', '#b5b5b5');
-        r.style.setProperty('--clr-input-error', '#eb77db');
-        r.style.setProperty('--clr-input-error-transparent', 'rgba(199, 26, 14, .2)');
-        r.style.setProperty('--clr-input-error-dark', '#270704');
-        r.style.setProperty('--clr-input-error-transparent-dark', 'rgba(39, 7, 4, .2)');
-        r.style.setProperty('--clr-gratos', '#0000ff');
-        r.style.setProperty('--clr-classique', '#00c3ff');
-        r.style.setProperty('--clr-fun', '#00bd00');
-        r.style.setProperty('--clr-choc', '#ffd900');
-        r.style.setProperty('--clr-style', '#ff0000');
-        r.style.setProperty('--clr-super', '#ff24ff');
-        r.style.setProperty('--clr-legendaire', '#8900ff');
-        r.style.setProperty('--clr-staff', '#ff7900');
-        r.style.setProperty('--clr-com', '#4BBF6B');
-        r.style.setProperty('--clr-sm', '#1e88e5');
-        r.style.setProperty('--clr-modo', '#009DDF');
-        r.style.setProperty('--clr-pm', '#6cd3ff');
-        r.style.setProperty('--clr-vip', '#ff24ff');
-        r.style.setProperty('--clr-status-weird', '#bb196a');
-        r.style.setProperty('--clr-status-danger', '#f44251');
-        r.style.setProperty('--clr-status-ban', '#201b1b');
-        r.style.setProperty('--tchat-border', 'rgb(0 0 0/80%');
-        r.style.setProperty('--tchat-message-hover', '#3e3e3e');
+            let body = document.querySelector('body');
 
+            // Apply the stylesheet to a document:
+            document.adoptedStyleSheets = [sheet];
+            body.style.backgroundColor = '#263238'
+
+            body.style.setProperty('background-image', 'unset');
+            navbar.style.setProperty('position', 'unset');
+            r.style.setProperty('--blue', 'lightblue');
+            r.style.setProperty('--clr-white', '#303030');
+            r.style.setProperty('--clr-black', '#b5b5b5');
+            r.style.setProperty('--clr-input-error', '#eb77db');
+            r.style.setProperty('--clr-input-error-transparent', 'rgba(199, 26, 14, .2)');
+            r.style.setProperty('--clr-input-error-dark', '#270704');
+            r.style.setProperty('--clr-input-error-transparent-dark', 'rgba(39, 7, 4, .2)');
+            r.style.setProperty('--clr-gratos', '#0000ff');
+            r.style.setProperty('--clr-classique', '#00c3ff');
+            r.style.setProperty('--clr-fun', '#00bd00');
+            r.style.setProperty('--clr-choc', '#ffd900');
+            r.style.setProperty('--clr-style', '#ff0000');
+            r.style.setProperty('--clr-super', '#ff24ff');
+            r.style.setProperty('--clr-legendaire', '#8900ff');
+            r.style.setProperty('--clr-staff', '#ff7900');
+            r.style.setProperty('--clr-com', '#4BBF6B');
+            r.style.setProperty('--clr-sm', '#1e88e5');
+            r.style.setProperty('--clr-modo', '#009DDF');
+            r.style.setProperty('--clr-pm', '#6cd3ff');
+            r.style.setProperty('--clr-vip', '#ff24ff');
+            r.style.setProperty('--clr-status-weird', '#bb196a');
+            r.style.setProperty('--clr-status-danger', '#f44251');
+            r.style.setProperty('--clr-status-ban', '#201b1b');
+            r.style.setProperty('--tchat-border', 'rgb(0 0 0/80%');
+            r.style.setProperty('--tchat-message-hover', '#3e3e3e');
+
+        }
     }
+
     sendResponse('Success');
 });
--- popup.js
+++ popup.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const coll = document.getElementsByClassName("collapsible");
+    let i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            const content = this.nextElementSibling;
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
+
     chrome.tabs.query({
         active: true,
         lastFocusedWindow: true
@@ -7,13 +22,55 @@ document.addEventListener('DOMContentLoaded', function() {
         if (tabs[0]['url'] !== 'https://www.superblablaland.com/tchat') {
             return false;
         }
+        if (localStorage.getItem('SbblTchatModPreferences')) {
+            const OldPrefs = JSON.parse(localStorage.getItem('SbblTchatModPreferences')).Preferences
+
+            Object.entries(OldPrefs).forEach(([key, value]) => {
+                if (typeof value === "boolean") {
+                    document.getElementById(key).checked = value;
+                } else {
+                    document.getElementById(key).value = value;
+                }
+            });
+        } else {
+            document.getElementById('nightMode').checked = true;
+            document.getElementById('autoScroll').checked = true;
+            document.getElementById('notifSound').checked = true;
+
+            document.getElementById('myMsgColor').value = '#00bcd4';
+            document.getElementById('pingMsg').value = '#1bc104';
+        }
+
         checkPageButton.addEventListener('click', function() {
+            let nightMode = document.getElementById('nightMode').checked;
+            let autoScroll = document.getElementById('autoScroll').checked;
+            let notifSound = document.getElementById('notifSound').checked;
+            let myMsgColor = document.getElementById('myMsgColor').value;
+            let pingMsg = document.getElementById('pingMsg').value;
+
+            let preferences = {
+                "Preferences" : {
+                    "nightMode" : nightMode,
+                    "autoScroll" : autoScroll,
+                    "notifSound" : notifSound,
+                    "myMsgColor" : myMsgColor,
+                    "pingMsg" : pingMsg,
+                }
+            }
+
             chrome.tabs.sendMessage(tabs[0]['id'],
                 {
                     command: "triggerTchatMod",
-                    title: "TchatMod"
+                    title: "TchatMod",
+                    data: preferences
                 },
+
                 function() {
+                    if (localStorage.getItem('SbblTchatModPreferences') === undefined) {
+                        localStorage.setItem('SbblTchatModPreferences', JSON.stringify(preferences));
+                    } else if (JSON.stringify(preferences) !== localStorage.getItem('SbblTchatModPreferences') ) {
+                        localStorage.setItem('SbblTchatModPreferences', JSON.stringify(preferences))
+                    }
                     console.log('Successfully started TchatMod');
                 });
         });
